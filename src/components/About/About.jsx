ecab62e0661397205eab08208ff886dbbbc95223
Guilherme Nunes <guilhermenunesbh5@gmail.com>
2022-09-05 11:27:44 -0300

    att about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -17,11 +17,13 @@ const About = () => {
                 <h1 className="a-title">Sobre mim</h1>
                 <p className="a-sub">Desenvolvedor Front-end</p>
                 <p className="a-desc">
-                    Em junho de 2021, comecei a cursar técnico em
-                    desenvolvimento de sistemas, e desde então venho descobrindo
-                    cada dia mais sobre o mundo da tecnologia, no momento estou
-                    desenvolvendo em React e estou em busca da minha primeira
-                    oportunidade no mercado.
+                    Me chamo Guilherme Nunes, tenho 19 anos e formei no ensino
+                    médio em 2020 na pandemia, no inicio de 2021 comecei a
+                    cursar inglês na UPTIME, e em junho de 2021, comecei a
+                    cursar técnico em desenvolvimento de sistemas no Senai, e
+                    desde então venho descobrindo cada dia mais sobre o mundo da
+                    tecnologia, no momento estou desenvolvendo em React e estou
+                    em busca da minha primeira oportunidade no mercado.
                 </p>
             </div>
         </div>

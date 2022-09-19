42300d1f47de5f16a2ee8805c304d5a94fa5498d
Guilherme Nunes <guilhermenunesbh5@gmail.com>
2022-09-19 13:13:54 -0300

    add projeto

--- src/data.js
+++ src/data.js
@@ -4,27 +4,29 @@ import Rotina from "./Img/rotina.png";
 import AppMercado from "./Img/app-mercado.png";
 import Buscador from "./Img/buscadorcep.png";
 import Lista from "./Img/lista-app.png";
+import NLWEsports from "./Img/NLW_eSports.png";
+import Aluracord from "./Img/Aluracord-matrix.png";
 
 export const products = [
     {
         id: 1,
-        img: Rotina,
-        link: "https://github.com/guilhermennf/organizacao-de-rotina",
+        img: NLWEsports,
+        link: "https://github.com/Guilhermennf/NLW_eSports",
     },
     {
         id: 2,
-        img: apiTmdb,
-        link: "https://github.com/guilhermennf/api-movies",
+        img: StarWars,
+        link: "https://github.com/Guilhermennf/StarWarsMovies",
     },
     {
         id: 3,
-        img: Lista,
-        link: "https://github.com/guilhermennf/tableapp",
+        img: Aluracord,
+        link: "https://github.com/Guilhermennf/Aluracord-matrix",
     },
     {
         id: 4,
-        img: StarWars,
-        link: "https://github.com/Guilhermennf/StarWarsMovies",
+        img: apiTmdb,
+        link: "https://github.com/guilhermennf/api-movies",
     },
     {
         id: 5,
@@ -35,5 +37,15 @@ export const products = [
         id: 6,
         img: Buscador,
         link: "https://github.com/guilhermennf/buscador-cep",
+    },
+    {
+        id: 7,
+        img: Rotina,
+        link: "https://github.com/guilhermennf/organizacao-de-rotina",
+    },
+    {
+        id: 8,
+        img: Lista,
+        link: "https://github.com/guilhermennf/tableapp",
     },
 ];
